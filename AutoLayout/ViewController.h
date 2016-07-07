3a2b08d8f55500177945d55b26e2f4985c2f0c72
millz60 <Matt@Matts-MacBook-Pro.local>
2016-07-07 17:19:20 -0500

    Auto-layout Practice
    Auto-layout Practice

---
+++ AutoLayout/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AutoLayout
+//
+//  Created by Matt Milner on 7/7/16.
+//  Copyright © 2016 Matt Milner. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
